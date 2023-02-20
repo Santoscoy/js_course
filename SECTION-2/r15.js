2cc70c760e3d12bcaefc1231c37eb762a362b165
Victor Santoscoy <victor.santoscoy@prontohousing.com>
2023-02-20 12:54:55 -0600

    Notes Section 2 R15

---
+++ SECTION-2/r15.js
@@ -0,0 +1,17 @@
+ 
+// las funciones pueden incluir metodos y propiedades
+ function a(){
+     console.log("Funcion a")
+ }
+
+ a();
+
+ a.nombre = "Pamela"
+ a.direccion = {
+     pais: "Mexico",
+     estado: "Chihuahua",
+     edificio: {
+         piso: 21,
+         nombre: "RIU"
+     }
+ }
