92db36638355946b718e7977cb960a4a254a326c
Victor Santoscoy <victor.santoscoy@prontohousing.com>
2023-02-21 11:02:56 -0600

    Notes Section 3 R28

---
+++ SECTION-3/r28.js
@@ -0,0 +1,21 @@
+// Objeto number
+
+
+var a = 100.4567889;
+
+console.log(a.toFixed(5));
+
+
+console.log(a);
+a = a.toPrecision(3);
+console.log(a);
+
+a = 10;
+a = a * "f"
+
+console.log(a);
+
+
+var b = new Number("20");
+console.log(b);
+console.log( b.valueOf() );
