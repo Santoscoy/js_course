cbbd2f96e708c3bfb712a7655375727d39b0c492
Victor Santoscoy <victor.santoscoy@prontohousing.com>
2023-02-20 19:10:22 -0600

    Notes Section 3 R25

---
+++ SECTION-3/r25.js
@@ -0,0 +1,22 @@
+//  Sobrecarga de operadores
+
+
+function crearProducto(nombre, precio){
+
+    nombre = nombre || "Sin nombre";
+    precio = precio || 0;
+
+    console.log( "Producto: ", nombre, "Precio: ", precio );
+}
+
+function crearProducto100(nombre ){
+    crearProducto(nombre, 100);
+}
+
+function crearProductoCamisa( precio ){
+    crearProducto("Camisa", precio);
+}
+
+crearProducto("Lapiz");  
+crearProducto100("Corrector");
+crearProductoCamisa(100);
