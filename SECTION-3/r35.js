81f83a213d3c8d49437a23e1468d9cf5a7e702dd
Victor Santoscoy <victor.santoscoy@prontohousing.com>
2023-02-22 17:59:47 -0600

    Notes Section 3 R35

---
+++ SECTION-3/r35.js
@@ -0,0 +1,17 @@
+// Expresiones regulares parte 2 
+
+var texto = "Aeropuerto"
+console.log(texto);
+
+// Busca vocales unidas al menos dos veces
+var arr = texto.match(/[aeiou]{2,2}/ig);
+console.log(arr);
+
+// grupos de dos 
+arr = texto.match(/\w{2,2}/ig);
+console.log(arr);
+
+// validacion para numeros
+texto = "La Respuesta de la suma es: 45 + 60 = 105";
+arr = texto.match(/\d{1,}|respuesta/ig);
+console.log(arr);
