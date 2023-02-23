a34ea49de54e4ceb503d46df90db0aa32541a579
Victor Santoscoy <victor.santoscoy@prontohousing.com>
2023-02-23 13:24:10 -0600

    Notes Section 4 r44

---
+++ SECTION-4/r45.js
@@ -0,0 +1,24 @@
+// Switch conditional
+
+var mes = 40;
+
+
+switch( mes ){
+
+    case ( 10 > 5)? 40: 1:
+        console.log("Enero");
+        break;
+    
+    case 2:
+        console.log("Febrero");
+        break;
+    
+    case 3:
+        console.log("Marzo");
+        break;
+
+    default:
+        console.log("Cualquier otro mes");
+        break;
+
+}
