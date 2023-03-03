9deb01bfa9d9fac21c03f9dbbd974046622ae030
Victor Santoscoy <victor.santoscoy@prontohousing.com>
2023-03-02 18:35:14 -0600

    0000

--- SECTION-3/r22.js
+++ SECTION-3/r22.js
@@ -1,6 +1,6 @@
 // Arreglos, colecciones de datos
 
-var arr = [5,4,3,2,1];
+var arr = [5, 4, 3, 2, 1];
 
 console.log(arr);
 
@@ -10,13 +10,13 @@ console.log(arr);
 
 // itera sobre los elementos y recibe como parametro una funcion que aplica
 // sobre los mismos
-arr = arr.map( function( elem ){
+arr = arr.map(function (elem) {
     elem *= elem;
     return elem;
-} );
+});
 console.log(arr)
 
-arr = arr.map( Math.sqrt );
+arr = arr.map(Math.sqrt);
 
 console.log(arr);
 
@@ -27,7 +27,7 @@ console.log(arr);
 // crea un arreglo de uns string, recibiendo como parametro el separador de los elementos
 arr = arr.split("|");
 console.log(arr);
- 
+
 // Agrega elementos al final del arreglo
 arr.push("6");
 console.log(arr);
@@ -46,12 +46,12 @@ console.log(arr, last);
 // primer parametro: index donde inicia
 // segundo parametro: index donde termina 
 // tercer...n parametros: elementos que remplezan a las posiciones anteriores
-arr.splice( 1, 0, "10" );
+arr.splice(1, 0, "10");
 console.log(arr);
 
 // primer parametro: posicion del arreglo donde empienza a cortar
 // segundo paramentro: posicion donde quiere terminar de cortar sin contarse asi mismo
 
-arr = arr.slice( 1, 3 );
+arr = arr.slice(1, 3);
 console.log(arr);
 
